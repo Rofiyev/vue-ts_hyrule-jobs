1fe95d278d06c8f29a487de4351bd070c049dd23
rof1yev <rofiyevdilshod@gmail.com>
2024-01-12 23:29:21 +0500

    #1. Component TS & data types &  methods type

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,30 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1>{{ title }} - {{ age }}</h1>
+    <button @click="changeName('Hello Abdulloh!', 20)">click me</button>
   </main>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'HomeView',
+  components: {},
+  data() {
+    return {
+      title: 'Hello Dilshod!' as string,
+      age: 18 as string | number
+    }
+  },
+  methods: {
+    changeName(name: string, age: number | string) {
+      this.title = name
+      this.age = age
+      return age
+    }
+  }
+})
+</script>
+
+<style scoped></style>
